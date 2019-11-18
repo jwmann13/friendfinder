48f98a6156a08078a88f035ef7d6c25dd5caabd9
Jeffrey Mann <jwmann22@gmail.com>
2019-11-17 22:16:50 -0500

    dynamically place questions from remote data on survey page

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,14 @@
 let users = require('../data/friend')
+let questions = require('../data/questions')
 
 module.exports = function(app) {
 
     app.get("/api/friends", (req, res) => {
         res.json(users);
+    });
+
+    app.get("/api/questions", (req, res) => {
+        res.json(questions);
     });
 
     app.post("/api/friends", (req, res) => {
