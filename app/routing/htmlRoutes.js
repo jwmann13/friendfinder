aa4ec6289dbb3409c8b967d3cd3645ac569764db
Jeffrey Mann <jwmann22@gmail.com>
2019-11-18 14:31:38 -0500

    added friends.html page to show all friends from friends.js, form posting is working

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -9,6 +9,10 @@ module.exports = function (app) {
         res.sendFile(path.join(__dirname, "../public/survey.html"))
     });
 
+    app.get('/friends', (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/friends.html"))
+    });
+
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, "../public/404.html"))
     })
