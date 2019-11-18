ba6ee2479c0b1fe1a93107fc3bd5827246a408e3
Jeffrey Mann <jwmann22@gmail.com>
2019-11-18 15:12:14 -0500

    added generic modal and dummy data to friends.js

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,12 +1,42 @@
 let users = [{
-    "name": "Jeff",
+    "name": "Scooby",
     "photo": "https://lorempixel.com/200/200/",
     "scores": [
         1,
+        3,
+        5
+    ]
+}, {
+    "name": "Shaggy",
+    "photo": "https://lorempixel.com/200/200/",
+    "scores": [
+        3,
+        4,
+        1
+    ]
+}, {
+    "name": "Velma",
+    "photo": "https://lorempixel.com/200/200/",
+    "scores": [
+        5,
+        1,
+        3
+    ]
+}, {
+    "name": "Daphne",
+    "photo": "https://lorempixel.com/200/200/",
+    "scores": [
         2,
         5,
-        5,
-        6
+        2
+    ]
+}, {
+    "name": "Fred",
+    "photo": "https://lorempixel.com/200/200/",
+    "scores": [
+        4,
+        4,
+        4
     ]
 }]
 
